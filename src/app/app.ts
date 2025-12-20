457cda2a58a671be00b3e263af03b7889ccc021d
OtavioXavier <otavioxaviermarinho@gmail.com>
2025-12-20 13:59:47 -0300

    ✨ feat[app]: integra seções de experiência e projetos no app principal

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,12 @@ import { Component } from '@angular/core';
 import { SectionList } from './components/section-list/section-list';
 import { ContactList } from './components/contact-list/contact-list';
 import { SobreSection } from './sections/sobre-section/sobre-section';
+import { ExperienciaSection } from './sections/experiencia-section/experiencia-section';
+import { ProjetosSection } from './sections/projetos-section/projetos-section';
 
 @Component({
   selector: 'app-root',
-  imports: [SectionList, ContactList, SobreSection],
+  imports: [SectionList, ContactList, SobreSection, ExperienciaSection, ProjetosSection],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })

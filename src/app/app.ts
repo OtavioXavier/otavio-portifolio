b246f7e6a468855e0343d109311623fc6635fccc
OtavioXavier <otavioxaviermarinho@gmail.com>
2025-12-20 11:12:05 -0300

    ♻️ refactor[app]: refatorar componente principal do portfólio

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,14 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { SectionList } from './components/section-list/section-list';
+import { ContactList } from './components/contact-list/contact-list';
+import { SobreSection } from './sections/sobre-section/sobre-section';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [SectionList, ContactList, SobreSection],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('otavio-portifolio');
+
 }

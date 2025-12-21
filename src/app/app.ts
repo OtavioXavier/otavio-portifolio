ea28bc8606a35b7cfaaf43b94505519eb1aeb6a2
OtavioXavier <otavioxaviermarinho@gmail.com>
2025-12-21 12:15:17 -0300

    💄 style[layout]: make main layout responsive for mobile devices

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,10 +4,11 @@ import { ContactList } from './components/contact-list/contact-list';
 import { SobreSection } from './sections/sobre-section/sobre-section';
 import { ExperienciaSection } from './sections/experiencia-section/experiencia-section';
 import { ProjetosSection } from './sections/projetos-section/projetos-section';
+import { HeaderSection } from './components/header-section/header-section';
 
 @Component({
   selector: 'app-root',
-  imports: [SectionList, ContactList, SobreSection, ExperienciaSection, ProjetosSection],
+  imports: [HeaderSection, SectionList, ContactList, SobreSection, ExperienciaSection, ProjetosSection],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })

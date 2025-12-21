b2310f0af8614acd5a798679aaf25ed53ded0b4f
OtavioXavier <otavioxaviermarinho@gmail.com>
2025-12-21 12:48:53 -0300

    ♻️ refactor[routes]: configurar rotas e adicionar router-outlet no app

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { CurriculoSection } from './sections/curriculo-section/curriculo-section';
+import { HomeSection } from './sections/home-section/home-section';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeSection
+  },
+  {
+    path: 'curriculo',
+    component: CurriculoSection
+  }
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,9 @@
 import { Component } from '@angular/core';
-import { SectionList } from './components/section-list/section-list';
-import { ContactList } from './components/contact-list/contact-list';
-import { SobreSection } from './sections/sobre-section/sobre-section';
-import { ExperienciaSection } from './sections/experiencia-section/experiencia-section';
-import { ProjetosSection } from './sections/projetos-section/projetos-section';
-import { HeaderSection } from './components/header-section/header-section';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderSection, SectionList, ContactList, SobreSection, ExperienciaSection, ProjetosSection],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })

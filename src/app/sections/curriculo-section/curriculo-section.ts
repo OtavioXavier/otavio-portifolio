8c43d98d66d3082f2e49e0a3b88908075c04cdeb
OtavioXavier <otavioxaviermarinho@gmail.com>
2025-12-21 12:48:51 -0300

    ✨ feat[curriculo]: adicionar componente para exibir currículo em pdf

---
+++ src/app/sections/curriculo-section/curriculo-section.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-curriculo-section',
+  imports: [],
+  templateUrl: './curriculo-section.html',
+  styleUrl: './curriculo-section.scss',
+})
+export class CurriculoSection {
+
+}

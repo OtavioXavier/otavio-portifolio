087bc9f64f3bbf31f48778650dc588acfad77be5
OtavioXavier <otavioxaviermarinho@gmail.com>
2026-04-10 15:36:53 -0300

    :sparkles: feat: adiciona lanterna

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { Component, HostListener, inject, PLATFORM_ID } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -8,5 +9,14 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.scss'
 })
 export class App {
+  private readonly platformId = inject(PLATFORM_ID);
 
+  @HostListener('document:mousemove', ['$event'])
+  onDocumentMouseMove(event: MouseEvent): void {
+    if (!isPlatformBrowser(this.platformId)) {
+      return;
+    }
+    document.documentElement.style.setProperty('--flashlight-x', `${event.clientX}px`);
+    document.documentElement.style.setProperty('--flashlight-y', `${event.clientY}px`);
+  }
 }
